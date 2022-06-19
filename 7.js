dae4e0769e8bfbb9c99cc61bbe67f5ba64ec5ecf
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-19 17:15:43 +0300

    Initital

--- 7.js
+++
@@ -1,5 +0,0 @@
-const a = [1,2,-6,5,15,35];
-
-let res = a.reduce((sum, n) => sum += n % 5 !== 0 ? n*n : 0);
-
-console.log(res);
--- 9.js
+++
@@ -1,4 +0,0 @@
-const a = [2,5,-5, 15, 33, 0, -99];
-const n = 8;
-
-console.log(a.filter(x=>x < n).length)
